c3ae9afd9d69b474543ca9eee545c6c46ea8391e
Hoa Nguyen <hoanguyendn61@gmail.com>
2021-03-22 15:11:46 +0700

    Add all files to github

---
+++ Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿
+namespace SecurityPanel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.lbAccessLog = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Security code";
+            // 
+            // txtCode
+            // 
+            this.txtCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCode.Location = new System.Drawing.Point(154, 25);
+            this.txtCode.MaxLength = 4;
+            this.txtCode.Name = "txtCode";
+            this.txtCode.PasswordChar = '●';
+            this.txtCode.ReadOnly = true;
+            this.txtCode.Size = new System.Drawing.Size(195, 30);
+            this.txtCode.TabIndex = 1;
+            this.txtCode.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lbAccessLog
+            // 
+            this.lbAccessLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAccessLog.FormattingEnabled = true;
+            this.lbAccessLog.ItemHeight = 25;
+            this.lbAccessLog.Location = new System.Drawing.Point(22, 309);
+            this.lbAccessLog.Name = "lbAccessLog";
+            this.lbAccessLog.Size = new System.Drawing.Size(372, 154);
+            this.lbAccessLog.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 281);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Access log:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(84, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(44, 43);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(143, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(44, 43);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(202, 93);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(44, 43);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(84, 154);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(44, 43);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(143, 154);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(44, 43);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(202, 154);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(44, 43);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(84, 217);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(44, 43);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(143, 217);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(44, 43);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(202, 217);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(44, 43);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button_Click);
+            // 
+            // button0
+            // 
+            this.button0.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button0.Location = new System.Drawing.Point(262, 217);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(44, 43);
+            this.button0.TabIndex = 13;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button_Click);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnter.Location = new System.Drawing.Point(262, 154);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(44, 43);
+            this.btnEnter.TabIndex = 14;
+            this.btnEnter.Text = "#";
+            this.btnEnter.UseVisualStyleBackColor = false;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(262, 93);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(44, 43);
+            this.btnClear.TabIndex = 15;
+            this.btnClear.Text = "C";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 484);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbAccessLog);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Security Panel";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.key_Press);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.ListBox lbAccessLog;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SecurityPanel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string line;
+            System.IO.StreamReader file = new System.IO.StreamReader(@"D:\Download\Study\hoc\YEAR 2\HOCKY II\dotNet\WinForm\SecurityPanel\bin\Debug\accesslog.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                lbAccessLog.Items.Add(line);
+            }
+            file.Close();
+        }
+       
+        private void button_Click(object sender, MouseEventArgs e)
+        {
+            txtCode.Text += ((Button)sender).Text;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtCode.Text = "";
+        }
+
+        private void key_Press(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = Char.IsDigit(e.KeyChar) && !(Char.IsControl(e.KeyChar));
+            if (e.Handled == true)
+            {
+                txtCode.Text += e.KeyChar.ToString();
+            }
+            if (e.KeyChar == (char)13)
+            {
+                btnEnter_Click(sender, e);
+            }
+
+        }
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            string accesslog;
+            switch (txtCode.Text)
+            {
+                case "1645":
+                    accesslog = DateTime.Now.ToString() + "     Technicians";
+                    lbAccessLog.Items.Add(accesslog);
+                    break;
+                case "1689":
+                    accesslog = DateTime.Now.ToString() + "     Technicians";
+                    lbAccessLog.Items.Add(accesslog);
+                    break;
+                case "8345":
+                    accesslog = DateTime.Now.ToString() + "     Custodians";
+                    lbAccessLog.Items.Add(accesslog);
+                    break;
+                case "9998":
+                    accesslog = DateTime.Now.ToString() + "     Scientists";
+                    lbAccessLog.Items.Add(accesslog);
+                    break;
+                case "1006":
+                    accesslog = DateTime.Now.ToString() + "     Scientists";
+                    lbAccessLog.Items.Add(accesslog);
+                    break;
+                case "1008":
+                    accesslog = DateTime.Now.ToString() + "     Scientists";
+                    lbAccessLog.Items.Add(accesslog);
+                    break;
+                default:
+                    accesslog = DateTime.Now.ToString() + "     Restricted Access!";
+                    lbAccessLog.Items.Add(accesslog);
+                    break;
+            }
+            txtCode.Text = "";
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.IO.StreamWriter SaveFile = new System.IO.StreamWriter("accesslog.txt");
+            foreach (var item in lbAccessLog.Items)
+            {
+                SaveFile.WriteLine(item);
+            }
+            SaveFile.Close();
+        }
+    }
+}
